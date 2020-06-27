0ed62d15da1a570866876aff4aab7b9826179f5f
Muhammad Azhan Awais <57989015+AzhanAwais@users.noreply.github.com>
2020-06-27 20:40:34 +0500

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,16 @@
+// var stack=[];
+
+function getChar(num)
+{
+    alert(num);
+    // if(char==="+" || char==="-" || char==="*" || char==="%" || char==="sqrt" || char==="sq" )
+    // {
+    //     var fpop=stack.pop();
+    //     alert(fpop);
+    // }
+    // else
+    // { 
+    //     alert(stack.push(char));
+    // }
+}
+
