ca60bd7235c7db25457bf747a129c58bc2aed7b9
Muhammad Azhan Awais <57989015+AzhanAwais@users.noreply.github.com>
2020-06-28 18:58:05 +0500

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,16 +1,47 @@
-// var stack=[];
-
-function getChar(num)
+function setTextFiledValue(num)
 {
-    alert(num);
-    // if(char==="+" || char==="-" || char==="*" || char==="%" || char==="sqrt" || char==="sq" )
-    // {
-    //     var fpop=stack.pop();
-    //     alert(fpop);
-    // }
-    // else
-    // { 
-    //     alert(stack.push(char));
-    // }
+    var text=document.getElementById('textfield');
+    text.value +=num;
 }
 
+function RemoveAndsetTextFiledValue(num)
+{
+    var text=document.getElementById('textfield');
+    text.value =num;
+}
+
+function calculateEquation()
+{
+    var text=document.getElementById('textfield');
+    var answer=eval(text.value);
+    text.value = answer;
+}
+
+function RemoveChar()
+{
+    var text=document.getElementById('textfield');
+    var len=text.value.length;
+    var AfterRemove=text.value.slice(0,len-1);
+    text.value=AfterRemove;
+}
+
+function clears()
+{
+    var text=document.getElementById('textfield');
+    text.value=" ";
+}
+
+
+function calculateRoot()
+{
+    var num=document.getElementById('textfield').value;
+    var ans=Math.sqrt(parseInt(num));
+    RemoveAndsetTextFiledValue(ans);
+}
+
+function calSquare()
+{
+    var num=document.getElementById('textfield').value;
+    var ans=parseInt(num*num);
+    RemoveAndsetTextFiledValue(ans);
+}
